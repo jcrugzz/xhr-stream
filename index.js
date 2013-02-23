3dc9cee4fa9565ab34d240c8410f43ab7c9f02ac
Jarrett Cruger <jcruger@qrhook.com>
2013-02-23 01:37:37 -0500

    Added request and response streams

--- index.js
+++ index.js
@@ -2,20 +2,17 @@
 //
 // Module for experimenting with XHR using a stream interface
 
-var readable = require('stream').Readable;
-var inherits = require('inherits');
+var Request = require('./lib/request');
 
 module.exports = function (options, cb) {
     return new xhr(options, cb);
 };
 
-inherits(xhr, readable);
 
 function xhr (options, cb) {
-    readable.call(this);
-
     var self = this;
-    this.xhr = new XMLHttpRequest();
+    this.xhr = request()
+
 
 }
 
