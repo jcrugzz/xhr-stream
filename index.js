c2eaaac50fbce190e9f3ee6413d06f8173cbc0e3
Jarrett Cruger <jcruger@qrhook.com>
2013-02-26 19:01:26 -0500

    Using Raynos's fork so it wont break when browserified

--- index.js
+++ index.js
@@ -7,20 +7,13 @@ var Request = require('./lib/request');
 module.exports = xhr;
 
 function xhr (options, cb) {
-    if (typeof options === 'string') {
-        var method = options;
-        options = {
-            method: method
-        }
-    } else if (!options) {
-        options = {};
-    }
+    if (!options) options = {};
     if (!options.host) options.host = window.location.host.split(':')[0];
     if (!options.port) options.port = window.location.port;
 
     var req = new Request(new window.XMLHttpRequest, options);
-    if (options.method.toLowerCase() === 'get') req.end();
     if (cb) req.on('response', cb);
+    if (options.method.toLowerCase() === 'get') req.end();
     return req;
 }
 
